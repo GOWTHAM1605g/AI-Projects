612138e8b06989ce0cc9587f953d66c92f1ecaed
GOWTHAM1605g <gowthamgowdam37@gmail.com>
2026-06-16 09:07:26 +0530

    Add Project1 main.py

---
+++ Project1/ai_greeting.py
@@ -0,0 +1,8 @@
+print("🤖 AI Greeting Assistant")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello,", name)
+print("You are", age, "years old.")
+print("Have a great day!")
